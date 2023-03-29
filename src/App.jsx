81d7f8c8d05d866639ac0e932d6cc94b0b3a5833
gmelenchon <gmelenchon@mbcloud.fr>
2023-03-29 09:18:39 +0200

    add dark & light mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
+import { useContext, useState } from "react";
 import { Level1 } from "./components/Level1";
+import { THEME, ThemeModeContext } from "./contexts/ThemeModeContext";
 
 export function App() {
+  const initialThemeMode = useContext(ThemeModeContext)
+
+  const [themeMode, setThemeMode] = useState(initialThemeMode)
+
   return (
-    <div>
+    <ThemeModeContext.Provider value={{themeMode, setThemeMode}}>
+    <div style={{color:THEME[themeMode].textColor, backgroundColor:THEME[themeMode].backgroundColor, height:'100vh'}}>
       <Level1 />
     </div>
+    </ThemeModeContext.Provider>
   );
 }
